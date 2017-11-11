94776a2e50d01787933bf278ae5b86a28c700d7f
Nuno Freitas <nunofreitas@gmail.com>
2017-11-11 18:12:59 +0000

    IllegalInheritanceError test

---
+++ src/remake/test/system/IllegalInheritanceErrorTest.ts
@@ -0,0 +1,19 @@
+import { IllegalInheritanceError } from '@main/system/index';
+import { expect } from 'chai';
+import { suite } from 'mocha-typescript';
+import { test } from 'mocha-typescript';
+
+/**
+ * {@link IllegalInheritanceError} test.
+ */
+@suite
+export class IllegalInheritanceErrorTest {
+    @test
+    public throwTest(): void {
+        expect(
+            () => { throw new IllegalInheritanceError('My custom message'); }
+        ).to.throw(
+            IllegalInheritanceError
+        );
+    }
+}
