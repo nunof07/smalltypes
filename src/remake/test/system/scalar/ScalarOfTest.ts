f99090410d34ba4eaf21f2d5da115586ce2f6411
Nuno Freitas <nunofreitas@gmail.com>
2017-11-12 11:22:04 +0000

    ScalarOf test

---
+++ src/remake/test/system/scalar/ScalarOfTest.ts
@@ -0,0 +1,30 @@
+import { ScalarOf } from '@main/system/scalar/index';
+import { expect } from 'chai';
+import { suite } from 'mocha-typescript';
+import { test } from 'mocha-typescript';
+
+/**
+ * {@link ScalarOf} test.
+ */
+@suite
+export class ScalarOfTest {
+    @test
+    public isTrue(): void {
+        expect(
+           new ScalarOf(true).value()
+        ).to.equal(
+            true,
+            'should be true'
+        );
+    }
+
+    @test
+    public isHelloWorld(): void {
+        expect(
+           new ScalarOf('HelloWorld').value()
+        ).to.equal(
+            'HelloWorld',
+            'should be HelloWorld'
+        );
+    }
+}
