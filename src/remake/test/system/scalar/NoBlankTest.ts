1ada4a21e0e550531bdf4d8a22f8df670c1c8095
Nuno Freitas <nunofreitas@gmail.com>
2017-11-12 11:15:15 +0000

    NoBlank test

---
+++ src/remake/test/system/scalar/NoBlankTest.ts
@@ -0,0 +1,48 @@
+import { IllegalStateError } from '@main/system/index';
+import { NoBlank } from '@main/system/scalar/index';
+import { ScalarOf } from '@main/system/scalar/index';
+import { expect } from 'chai';
+import { suite } from 'mocha-typescript';
+import { test } from 'mocha-typescript';
+
+/**
+ * {@link NoBlank} test.
+ */
+@suite
+export class NoBlankTest {
+    @test
+    public nullTest(): void {
+        expect(
+            () => new NoBlank(
+                new ScalarOf(null)
+            )
+            .value()
+        ).to.throw(
+            IllegalStateError
+        );
+    }
+
+    @test
+    public undefinedTest(): void {
+        expect(
+            () => new NoBlank(
+                new ScalarOf(undefined)
+            )
+            .value()
+        ).to.throw(
+            IllegalStateError
+        );
+    }
+
+    @test
+    public stringTest(): void {
+        expect(
+            () => new NoBlank(
+                new ScalarOf('hello')
+            )
+            .value()
+        ).to.not.throw(
+            IllegalStateError
+        );
+    }
+}
