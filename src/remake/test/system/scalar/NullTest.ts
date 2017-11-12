0ff708661b3661ff1edba1278424796cf29056f2
Nuno Freitas <nunofreitas@gmail.com>
2017-11-12 11:19:16 +0000

    Null test

---
+++ src/remake/test/system/scalar/NullTest.ts
@@ -0,0 +1,20 @@
+import { Null } from '@main/system/scalar/index';
+import { expect } from 'chai';
+import { suite } from 'mocha-typescript';
+import { test } from 'mocha-typescript';
+
+/**
+ * {@link Null} test.
+ */
+@suite
+export class NullTest {
+    @test
+    public isNull(): void {
+        expect(
+           new Null().value()
+        ).to.equal(
+            null,
+            'should be null'
+        );
+    }
+}
