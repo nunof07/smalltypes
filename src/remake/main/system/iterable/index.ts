59d57b0a0f425802020f4ff2d3258ccc1f84791c
Nuno Freitas <nunofreitas@gmail.com>
2017-11-12 19:49:47 +0000

    First

--- src/remake/main/system/iterable/index.ts
+++ src/remake/main/system/iterable/index.ts
@@ -3,6 +3,7 @@
  */
 export * from './Equals';
 export * from './Filtered';
+export * from './First';
 export * from './LengthOf';
 export * from './Limited';
 export * from './Mapped';
---
+++ src/remake/test/system/iterable/FirstTest.ts
@@ -0,0 +1,55 @@
+import { First } from '@main/system/iterable/index';
+import { Scalar } from '@main/system/scalar/index';
+import { expect } from 'chai';
+import { suite } from 'mocha-typescript';
+import { test } from 'mocha-typescript';
+
+/**
+ * {@link First} test.
+ */
+@suite
+export class FirstTest {
+    @test.only
+    public firstValue(): void {
+        expect(
+            new First([1, 2, 3]).value().value
+        ).to.equal(
+            1
+        );
+    }
+
+    @test.only
+    public afterIterate(): void {
+        expect(
+            ((): number => {
+                const iterable: Iterable<number> = [1, 2, 3];
+                const next: Scalar<IteratorResult<number>> = new First(iterable);
+                const limit: number = 2;
+                let count: number = 0;
+
+                // tslint:disable-next-line:variable-name
+                for (const _item of iterable) {
+                    count += 1;
+
+                    if (count >= limit) {
+                        break;
+                    }
+                }
+
+                return next.value().value;
+            })()
+        ).to.equal(
+            1
+        );
+    }
+
+    @test.only
+    public emptyIterable(): void {
+        expect(
+            new First([]).value().done
+        ).to.equal(
+            true,
+            'Next on empty iterable must return done'
+        );
+    }
+}
