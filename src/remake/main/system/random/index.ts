3938308c19a0b0d61ed94a14ec0aa41f654c8707
Nuno Freitas <nunofreitas@gmail.com>
2017-11-18 11:38:18 +0000

    Remove Crypto and Randomize

--- src/remake/main/system/random/index.ts
+++ src/remake/main/system/random/index.ts
@@ -1,16 +1,6 @@
 /**
  * Index.
  */
-export * from './Crypto';
-export * from './MsCrypto';
 export * from './ParkMillerRandom';
 export * from './Random';
-export * from './Randomize';
-export * from './RandomizedInt';
-export * from './RandomizedInt8';
-export * from './RandomizedInt16';
-export * from './RandomizedInt32';
-export * from './RandomizedUint8';
-export * from './RandomizedUint16';
-export * from './RandomizedUint32';
 export * from './RandomOf';
