d0da250696a4332ed24030691d0a15ceeb432b88
Nuno Freitas <nunofreitas@gmail.com>
2017-11-18 17:27:22 +0000

    Cached test

---
+++ src/remake/test/system/scalar/CachedTest.ts
@@ -0,0 +1,32 @@
+import { ParkMillerRandom } from '@main/system/random/index';
+import { Cached } from '@main/system/scalar/index';
+import { Rounded } from '@main/system/scalar/index';
+import { ResultOf } from '@main/system/scalar/index';
+import { ScalarOf } from '@main/system/scalar/index';
+import { expect } from 'chai';
+import { suite } from 'mocha-typescript';
+import { test } from 'mocha-typescript';
+
+/**
+ * {@link Cached} test.
+ */
+@suite
+export class CachedTest {
+    @test
+    public cachesValue(): void {
+        const random: ParkMillerRandom = new ParkMillerRandom(new ScalarOf(1337));
+        const cached: Cached<number> = new Cached(
+            new ResultOf((): number => random.next())
+        );
+        const expected: number = new Rounded(new ScalarOf(0.010463855699852042), 10).value();
+        expect([
+            new Rounded(cached, 10).value(),
+            new Rounded(cached, 10).value(),
+            new Rounded(cached, 10).value()
+        ]).to.be.equalTo([
+            expected,
+            expected,
+            expected
+        ]);
+    }
+}
