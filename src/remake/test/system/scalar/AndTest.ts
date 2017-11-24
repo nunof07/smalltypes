4c9dd4019fd6dafe4b231307a6c343ca73f0bc97
Nuno Freitas <nunofreitas@gmail.com>
2017-11-24 21:56:00 +0000

    Conditions tests

--- src/remake/test/system/scalar/AndTest.ts
+++ src/remake/test/system/scalar/AndTest.ts
@@ -13,14 +13,42 @@ export class AndTest {
     @test
     public isTrue(): void {
         expect(
-            new And(new True(), new True()).value()
+            new And(true, true).value()
         ).to.equal(true, 'When all conditions are true must return true');
     }
 
     @test
     public isFalse(): void {
         expect(
+            new And(true, false).value()
+        ).to.equal(false, 'When at least one condition is false must return false (Scalar)');
+    }
+
+    @test
+    public isTrueScalar(): void {
+        expect(
+            new And(new True(), new True()).value()
+        ).to.equal(true, 'When all conditions are true must return true');
+    }
+
+    @test
+    public isFalseScalar(): void {
+        expect(
             new And(new True(), new False()).value()
-        ).to.equal(false, 'When at least one condition is false must return false');
+        ).to.equal(false, 'When at least one condition is false must return false (Scalar)');
+    }
+
+    @test
+    public isTrueFunction(): void {
+        expect(
+            new And((): boolean => true, (): boolean => true).value()
+        ).to.equal(true, 'When all conditions are true must return true (Function)');
+    }
+
+    @test
+    public isFalsFunctione(): void {
+        expect(
+            new And((): boolean => true, (): boolean => false).value()
+        ).to.equal(false, 'When at least one condition is false must return false (Function)');
     }
 }
--- src/remake/test/system/scalar/OrTest.ts
+++ src/remake/test/system/scalar/OrTest.ts
@@ -13,14 +13,42 @@ export class OrTest {
     @test
     public isTrue(): void {
         expect(
-            new Or(new False(), new True()).value()
+            new Or(false, true).value()
         ).to.equal(true, 'When at least one true condition must return true');
     }
 
     @test
     public isFalse(): void {
         expect(
-            new Or(new False(), new False()).value()
+            new Or(false, false).value()
         ).to.equal(false, 'When all conditions are false must return false');
+    }
+
+    @test
+    public isTrueScalar(): void {
+        expect(
+            new Or(new False(), new True()).value()
+        ).to.equal(true, 'When at least one true condition must return true (Scalar)');
+    }
+
+    @test
+    public isFalseScalar(): void {
+        expect(
+            new Or(new False(), new False()).value()
+        ).to.equal(false, 'When all conditions are false must return false (Scalar)');
+    }
+
+    @test
+    public isTrueFunction(): void {
+        expect(
+            new Or((): boolean => false, (): boolean => true).value()
+        ).to.equal(true, 'When at least one true condition must return true (Function)');
+    }
+
+    @test
+    public isFalseFunction(): void {
+        expect(
+            new Or((): boolean => false, (): boolean => false).value()
+        ).to.equal(false, 'When all conditions are false must return false (Function)');
     }
 }
