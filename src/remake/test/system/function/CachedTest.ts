6544e39e383e2ff7a3ce1345222922eb1d1310b8
Nuno Freitas <nunofreitas@gmail.com>
2017-11-24 22:00:30 +0000

    Refactor Cached tests to use sum function

--- src/remake/test/system/function/CachedTest.ts
+++ src/remake/test/system/function/CachedTest.ts
@@ -1,8 +1,5 @@
 import { Cached } from '@main/system/function/index';
 import { FunctionOf } from '@main/system/function/index';
-import { ParkMillerRandom } from '@main/system/random/index';
-import { Rounded } from '@main/system/scalar/index';
-import { ScalarOf } from '@main/system/scalar/index';
 import { expect } from 'chai';
 import { suite } from 'mocha-typescript';
 import { test } from 'mocha-typescript';
@@ -14,23 +11,22 @@ import { test } from 'mocha-typescript';
 export class CachedTest {
     @test
     public cachesResults(): void {
-        const random: ParkMillerRandom = new ParkMillerRandom(new ScalarOf(1337));
+        let sum: number = 0;
         const cached: Cached<boolean, number> = new Cached(
-            new FunctionOf((): number => random.next())
-        );
-        const expected: number = round(0.010463855699852042);
-        expect([
-            round(cached.apply(true)),
-            round(cached.apply(true)),
-            round(cached.apply(true))
-        ]).to.be.equalTo([
-            expected,
-            expected,
-            expected
-        ]);
+            new FunctionOf((): number => {
+                sum += 10;
 
-        function round(n: number): number {
-            return new Rounded(n, 10).value();
-        }
+                return sum;
+            })
+        );
+        expect([
+            cached.apply(true),
+            cached.apply(true),
+            cached.apply(true)
+        ]).to.be.equalTo([
+            10,
+            10,
+            10
+        ]);
     }
 }
--- src/remake/test/system/scalar/CachedTest.ts
+++ src/remake/test/system/scalar/CachedTest.ts
@@ -1,7 +1,4 @@
-import { ParkMillerRandom } from '@main/system/random/index';
 import { Cached } from '@main/system/scalar/index';
-import { Rounded } from '@main/system/scalar/index';
-import { ScalarOf } from '@main/system/scalar/index';
 import { expect } from 'chai';
 import { suite } from 'mocha-typescript';
 import { test } from 'mocha-typescript';
@@ -13,17 +10,20 @@ import { test } from 'mocha-typescript';
 export class CachedTest {
     @test
     public cachesValue(): void {
-        const random: ParkMillerRandom = new ParkMillerRandom(new ScalarOf(1337));
-        const cached: Cached<number> = new Cached((): number => random.next());
-        const expected: number = new Rounded(0.010463855699852042, 10).value();
+        let sum: number = 0;
+        const cached: Cached<number> = new Cached((): number => {
+            sum += 10;
+
+            return sum;
+        });
         expect([
-            new Rounded(cached, 10).value(),
-            new Rounded(cached, 10).value(),
-            new Rounded(cached, 10).value()
+            cached.value(),
+            cached.value(),
+            cached.value()
         ]).to.be.equalTo([
-            expected,
-            expected,
-            expected
+            10,
+            10,
+            10
         ]);
     }
 }
