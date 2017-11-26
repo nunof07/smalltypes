e7cdde07af598d5a7625964460ee9b3431d21e2d
Nuno Freitas <nunofreitas@gmail.com>
2017-11-26 17:19:56 +0000

    Main that exports all types

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+/**
+ * Main.
+ */
+import * as func from '@main/function/index';
+import * as iterable from '@main/iterable/index';
+import * as random from '@main/random/index';
+import * as scalar from '@main/scalar/index';
+
+export * from '@main/index';
+
+export {
+    func,
+    iterable,
+    random,
+    scalar
+};
